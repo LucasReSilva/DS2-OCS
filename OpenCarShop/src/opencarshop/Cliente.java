4231aa941f1d4863a3c8796b8a8a38d0b2e13f3c
Lucas Silva <lucasre.silva@hotmail.com>
2016-10-21 20:45:03 -0300

    Adição de algumas assinaturas de metodos e modificações no arquivo de documentação

--- OpenCarShop/src/opencarshop/Cliente.java
+++ OpenCarShop/src/opencarshop/Cliente.java
@@ -1,6 +1,8 @@
 package opencarshop;
 
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Cliente {
 
@@ -14,6 +16,25 @@ public class Cliente {
 
 	private String Telefone1;
 
-	private String Telefone2;
+	private String Telefone2;        
+        
+        private static boolean validarCPF(String cpf){
+            
+            
+            return false;            
+        }
 
+        private static boolean validarEmail(String email){
+            
+            final String PADRAO_EMAIL = 
+        "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+            
+            final Pattern PADRAO = Pattern.compile(PADRAO_EMAIL, Pattern.CASE_INSENSITIVE);
+            
+            Matcher casador;
+            casador = PADRAO.matcher(email);
+            
+            return casador.matches(); 
+        }
 }
--- OpenCarShop/src/opencarshop/fornecedor/Fornecedor.java
+++ OpenCarShop/src/opencarshop/fornecedor/Fornecedor.java
@@ -1,5 +1,8 @@
 package opencarshop.fornecedor;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Fornecedor {
 
 	private String CNPJ;
@@ -8,8 +11,33 @@ public class Fornecedor {
 
 	private String Descricao;
 
+        private String Email;
+        
 	private String Telefone1;
 
 	private String Telefone2;
+        
+        private static final String EMAIL_PATTERN = 
+        "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
+        private static boolean validarCNPJ(String cnpj){
+            
+            
+            return false;           
+        }
+        
+        private static boolean validarEmail(String email){
+            
+            final String PADRAO_EMAIL = 
+        "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+            
+            final Pattern PADRAO = Pattern.compile(PADRAO_EMAIL, Pattern.CASE_INSENSITIVE);
+            
+            Matcher casador;
+            casador = PADRAO.matcher(email);
+            
+            return casador.matches(); 
+        }
 }
