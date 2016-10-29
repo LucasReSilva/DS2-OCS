0adb9c74afda5b3f0fba17a552ab8fa81e26716e
Lucas Silva <lucasre.silva@hotmail.com>
2016-10-28 22:40:44 -0300

    Projeto limpo

--- Tentativa/src/opencarshop/OpenCarShop.java
+++ Tentativa/src/opencarshop/OpenCarShop.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -19,11 +20,21 @@ public class OpenCarShop extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("TelaPrincipal.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/opencarshop/view/Login.fxml"));        
         
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root); 
+        // Icone
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/recursos/icones/account-circle-white.png")));
         
+        // Desabilita maximizar
+        stage.setResizable(false);
+
+        // Titulo da janela
+        stage.setTitle("Login");
+
         stage.setScene(scene);
+        stage.setMaxHeight(768);
+        stage.setMaxWidth(1024);
         stage.show();
     }
 
