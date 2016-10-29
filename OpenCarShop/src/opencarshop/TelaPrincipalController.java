9b748ca24b6f019efc99328fb1ab6d6dc7b3b3f2
Lucas Silva <lucasre.silva@hotmail.com>
2016-10-29 19:11:21 -0300

    Agora fica visivel na parte inferior da tela em qual formulario o usuario esta

--- OpenCarShop/src/opencarshop/TelaPrincipalController.java
+++ OpenCarShop/src/opencarshop/TelaPrincipalController.java
@@ -15,6 +15,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 
 /**
  *
@@ -23,11 +24,15 @@ import javafx.scene.layout.StackPane;
 public class TelaPrincipalController implements Initializable {
     
     @FXML
-    private StackPane acContent;     
+    private StackPane acContent;   
+    
+    @FXML
+    private Text tx_info; 
     
     @FXML
     private void cadastrarFuncionario(ActionEvent event) {
         System.out.println("Cadastrar Funcionario");
+        tx_info.setText("Cadastrar Funcionario");
         FXMLLoader fxmlLoader = new FXMLLoader();
         try {
             fxmlLoader.load(getClass().getResource("/opencarshop/funcionario/view/Cadastrar.fxml").openStream());
@@ -42,6 +47,7 @@ public class TelaPrincipalController implements Initializable {
     @FXML
     private void listarFuncionario(ActionEvent event) {
         System.out.println("Buscar Funcionario");
+        tx_info.setText("Listar Funcionario");
         FXMLLoader fxmlLoader = new FXMLLoader();
         try {
             fxmlLoader.load(getClass().getResource("/opencarshop/funcionario/view/Buscar.fxml").openStream());
@@ -56,6 +62,7 @@ public class TelaPrincipalController implements Initializable {
     @FXML
     private void cadastrarFornecedor(ActionEvent event) {
         System.out.println("Cadastrar Fornecedor");
+        tx_info.setText("Cadastrar Fornecedor");
         FXMLLoader fxmlLoader = new FXMLLoader();
         try {
             fxmlLoader.load(getClass().getResource("/opencarshop/fornecedor/view/Cadastrar.fxml").openStream());
@@ -70,6 +77,7 @@ public class TelaPrincipalController implements Initializable {
     @FXML
     private void listarFornecedor(ActionEvent event) {
         System.out.println("Buscar Fornecedor");
+        tx_info.setText("Listar Fornecedor");
         FXMLLoader fxmlLoader = new FXMLLoader();
         try {
             fxmlLoader.load(getClass().getResource("/opencarshop/fornecedor/view/Buscar.fxml").openStream());
@@ -84,6 +92,7 @@ public class TelaPrincipalController implements Initializable {
     @FXML
     private void cadastrarCliente(ActionEvent event) {
         System.out.println("Cadastrar Cliente");
+        tx_info.setText("Cadastrar Cliente");
         FXMLLoader fxmlLoader = new FXMLLoader();
         try {
             fxmlLoader.load(getClass().getResource("/opencarshop/cliente/view/Cadastrar.fxml").openStream());
@@ -98,6 +107,7 @@ public class TelaPrincipalController implements Initializable {
     @FXML
     private void listarCliente(ActionEvent event) {
         System.out.println("Buscar Cliente");
+        tx_info.setText("Listar Cliente");
         FXMLLoader fxmlLoader = new FXMLLoader();
         try {
             fxmlLoader.load(getClass().getResource("/opencarshop/cliente/view/Buscar.fxml").openStream());
