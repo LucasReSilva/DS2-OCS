8e36200a42bb04fe61979c5c1ff1a0c2cd1f43ae
Tharlysson Breno <tharlyssonbreno@gmail.com>
2016-10-29 12:46:43 -0300

    criação da tela principal

--- OpenCarShop/src/opencarshop/TelaPrincipalController.java
+++ OpenCarShop/src/opencarshop/TelaPrincipalController.java
@@ -5,26 +5,40 @@
  */
 package opencarshop;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
 /**
  *
  * @author lucas
  */
 public class TelaPrincipalController implements Initializable {
+    @FXML
+    private StackPane acContent;       
+    @FXML
+    private void cadastrarFuncionario(ActionEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        try {
+            fxmlLoader.load(getClass().getResource("/opencarshop/funcionario/view/Cadastrar.fxml").openStream());
+        } catch (IOException e) {
+            
+        }
+        AnchorPane root = fxmlLoader.getRoot();
+        acContent.getChildren().clear();
+        acContent.getChildren().add(root);
+    }
     
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void listarFuncionario(ActionEvent event) {
+        System.out.println("listgem de funcionario");
     }
     
     @Override
