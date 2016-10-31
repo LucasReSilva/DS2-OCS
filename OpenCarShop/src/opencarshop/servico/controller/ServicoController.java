aef66d2957f4353064117803e3099cce03d9bf82
Tharlysson Breno <tharlyssonbreno@gmail.com>
2016-10-30 21:28:32 -0300

    finalização da criacao de servico

--- OpenCarShop/src/opencarshop/servico/controller/ServicoController.java
+++ OpenCarShop/src/opencarshop/servico/controller/ServicoController.java
@@ -1,6 +1,7 @@
 package opencarshop.servico.controller;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
@@ -36,7 +37,13 @@ public class ServicoController implements Initializable {
     private CheckBox chkValue;
     
     @FXML
+    private TextField tfid;
+    
+    @FXML
     private Label  labelErroServ;
+    
+    @FXML
+    private Label confirmaAtualizacao;
 
 //Colunas da tabela listar serviços
     
@@ -53,79 +60,99 @@ public class ServicoController implements Initializable {
   //metodos
     
     @FXML    
-  public void cadastraServico(ActionEvent event){
-    //istancia objeto serviço para configuraçao de atributos
-    Servico servico; 
-    //instacia objeto para inserção de objeto cadasrtado no banco
-    ServicoDAO servDao = new ServicoDAO();
-    
-    String descri=tfdescricao.getText();
-    String valor = tfvalor.getText();
-    
-    //Testando valores do cadastro
-    
-    if ((descri.length() > (int)45) || ("".equals(descri))  ){
-        labelErroServ.setText("Descrição deve ter até 45 caracteres");
-    
-    }
-    if (Pattern.matches("[a-zA-Z]+",valor) == true) {
-            labelErroServ.setText("por favor insira apenas numeros e virgula/ponto");
-}
-    
-    //passando para double
-    valor = valor.replace(",", ".");
-    Double valorDouble = Double.parseDouble(valor);
-    
-   
-    //Criando objeto serviço
-    servico =new Servico(descri,valorDouble,chkValue.isSelected());
-    
-    if( !servDao.insertServico(servico)){
-        
-        labelErroServ.setText("Serviço cadastrado...retornando ");
-    
-    }
-    
-    
-    
-    
-    
+    public void cadastraServico(ActionEvent event){
+        //istancia objeto serviço para configuraçao de atributos
+        Servico servico; 
+        //instacia objeto para inserção de objeto cadasrtado no banco
+        ServicoDAO servDao = new ServicoDAO();
 
-  }
+        String descri=tfdescricao.getText();
+        String valor = tfvalor.getText();
+
+        //Testando valores do cadastro
+
+        if ((descri.length() > (int)45) || ("".equals(descri))  ){
+            labelErroServ.setText("Descrição deve ter até 45 caracteres");
+
+        }
+        if (Pattern.matches("[a-zA-Z]+",valor) == true) {
+                labelErroServ.setText("por favor insira apenas numeros e virgula/ponto");
+        }
+
+        //passando para double
+        valor = valor.replace(",", ".");
+        Double valorDouble = Double.parseDouble(valor);
+
+
+        //Criando objeto serviço
+        servico =new Servico(descri,valorDouble,chkValue.isSelected());
+
+        if( !servDao.insertServico(servico)){
+
+            labelErroServ.setText("Serviço cadastrado...retornando ");
+
+        }
+    }
  
 
-@FXML
-private void carregaTabelaServico() throws Exception{
-    
-    col_descricao.setCellValueFactory(new PropertyValueFactory<>("descrição"));
-    col_valor.setCellValueFactory(new PropertyValueFactory<>("valorP"));
-    col_tpvalor.setCellValueFactory(new PropertyValueFactory<>("valorF"));    
+    @FXML
+    private void carregaTabelaServico() throws Exception{
 
-        ServicoDAO serv = new ServicoDAO();
-        List<Servico> listaServico = serv.getAllServicos();
-        ObservableList<Servico> observableListServico;
+        col_descricao.setCellValueFactory(new PropertyValueFactory<>("descricao"));
+        col_valor.setCellValueFactory(new PropertyValueFactory<>("valorP"));
+        col_tpvalor.setCellValueFactory(new PropertyValueFactory<>("valorF"));    
 
-        observableListServico = FXCollections.observableArrayList(listaServico);
-        tbl_servico.setItems(observableListServico);
+            ServicoDAO serv = new ServicoDAO();
+            List<Servico> listaServico = serv.getAllServicos();
+            ObservableList<Servico> observableListServico;
+
+            observableListServico = FXCollections.observableArrayList(listaServico);
+            tbl_servico.setItems(observableListServico);
+    }
+
+    @FXML
+    public void alterarServico(ActionEvent event) throws SQLException
+    {
+        Servico srv = new Servico();
         
+        srv.setId(Integer.valueOf(tfid.getText()));
+        srv.setDescricao(tfdescricao.getText());
+        srv.setValorPadrao(Double.valueOf(tfvalor.getText()));
+        srv.setValorFixo(chkValue.isSelected());
+        
+        ServicoDAO s = new ServicoDAO();
+        
+        if(s.alteraServico(srv))
+        {
+            confirmaAtualizacao.setText("Alteração realizada com sucesso!!");
+        }
+        else
+        {
+            confirmaAtualizacao.setText("Erro ao realizar a alteração!!");
+        }
+        
+    }
 
-}
-
-@FXML
-    public void buscar(){
+    public void selecionarItemTablelaServico(Servico servico){
+        if (servico.getDescricao() != null) {
+              tfdescricao.setText(servico.getDescricao());
+              tfvalor.setText(String.valueOf(servico.getValorPadrao()));
+              chkValue.setSelected(servico.getValorFixo());
+              tfid.setText(String.valueOf(servico.getId()));
+        } 
     }
 
 @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
-            System.out.println("Chamou");
+            //System.out.println("Chamou");
             carregaTabelaServico();
+            tbl_servico.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> selecionarItemTablelaServico(newValue));
         } catch (Exception ex) {
             
         }
         
     }
-
-
 
 }
--- OpenCarShop/src/opencarshop/servico/model/Servico.java
+++ OpenCarShop/src/opencarshop/servico/model/Servico.java
@@ -2,23 +2,62 @@ package opencarshop.servico.model;
 
 public class Servico {
     
-    private String descrição;
-    private double valorPadrão;
+    private String descricao;
+    private double valorPadrao;
     private  boolean valorFixo;
+    private int id;
+    
+    
     private String valorF;
     private String valorP;
+    
     public Servico(){
     }
     
     public Servico(String desc, double valor, boolean bfixo ){
         
-        this.descrição=desc;
-        this.valorPadrão=valor;
+        this.descricao=desc;
+        this.valorPadrao=valor;
         this.valorFixo=bfixo;
         
-    
-    
     }
+    
+    public String getDescricao() {
+        return descricao;
+    }
+    
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    
+    public double getValorPadrao() {
+        return valorPadrao;
+    }
+
+    public void setValorPadrao(double valorPadrao) {
+        this.valorPadrao = valorPadrao;
+    }
+
+    public boolean getValorFixo() {
+        return valorFixo;
+    }
+
+    public void setValorFixo(boolean valorFixo) {
+        this.valorFixo = valorFixo;
+    }    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+    
+    
     
     public String getValorP(){
         return valorP;
@@ -28,7 +67,6 @@ public class Servico {
         String v2=Double.toString(v).replace(".", ",");
         this.valorP="R$ "+v2;
     }
-    
     
     public String getValorF(){
         return valorF;
@@ -41,36 +79,4 @@ public class Servico {
             this.valorF="Não";
         }
     }
-    public String getDescrição() {
-        return descrição;
-    }
-
-    
-    
-    
-    public void setDescrição(String descrição) {
-        this.descrição = descrição;
-    }
-
-    public double getValorPadrão() {
-        return valorPadrão;
-    }
-
-    public void setValorPadrão(double valorPadrão) {
-        this.valorPadrão = valorPadrão;
-    }
-
-    public boolean isValorFixo() {
-        return valorFixo;
-    }
-
-    public void setValorFixo(boolean valorFixo) {
-        this.valorFixo = valorFixo;
-    }
-    
-    
-    
-    
-    
-    
 }
