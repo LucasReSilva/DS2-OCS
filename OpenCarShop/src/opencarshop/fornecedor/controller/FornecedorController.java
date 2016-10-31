4e6191c0a86814f1305d16210681ddd1e578e534
Lucas Silva <lucasre.silva@hotmail.com>
2016-10-31 10:26:19 -0300

    Fornecedor

--- OpenCarShop/src/opencarshop/fornecedor/controller/FornecedorController.java
+++ OpenCarShop/src/opencarshop/fornecedor/controller/FornecedorController.java
@@ -9,6 +9,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
@@ -23,17 +24,12 @@ import opencarshop.util.Utilidades;
 
 public class FornecedorController implements Initializable {
 
-    /**
+       /**
      * Initializes the controller class.
      */
-    // TELA DE AUTENTICACAO
-    @FXML
-    private Label labelErro;
-    @FXML
-    private TextField tf_cpf;
-    @FXML
-    private Hyperlink cadastroLink;
-
+   
+    
+    
     // TELA DE CADASTRO
     @FXML
     private TextField tf_cnpjCadastro;
@@ -41,13 +37,15 @@ public class FornecedorController implements Initializable {
     private TextField tf_razaoCadastro;
     @FXML
     private TextField tf_descricaoCadastro;
-
+    
+  
+    
     @FXML
     private ComboBox<String> cb_tipoCadastro;
     @FXML
     private TextField tf_emailCadastro;
     @FXML
-    private TextField tf_telefone1Cadastro;
+    private TextField tf_telefone1Cadastro;   
     @FXML
     private TextField tf_telefone2Cadastro;
     @FXML
@@ -64,10 +62,13 @@ public class FornecedorController implements Initializable {
     private TextField tf_numeroCadastro;
     @FXML
     private TextField tf_complementoCadastro;
-
+    @FXML
+    private CheckBox cb_ativo;
+    @FXML
+    private Label confirmaAlteracao;
     @FXML
     private Label resultadoCadastro;
-
+    
     // TABELA Fornecedor
     @FXML
     private TableColumn<Fornecedor, String> col_cnpj;
@@ -81,62 +82,19 @@ public class FornecedorController implements Initializable {
     private TableColumn<Fornecedor, String> col_telefone2;
     @FXML
     private TableColumn<Fornecedor, String> col_descricao;
+    
+    
     @FXML
     private TableView<Fornecedor> tbl_fornecedor;
-
-    /*  
-    @FXML
-    private void autenticar(ActionEvent event) 
-    {
-        Funcionario funcionario;
-        FuncionarioDAO func = new FuncionarioDAO();
-        funcionario = func.getFuncionario(tf_cpf.getText());
-        
-        if(funcionario.getCpf() != null)
-        {
-            if(funcionario.getSenha().equals(pf_senha.getText()))
-            {
-                Parent root = null;
-                try 
-                {
-                    root = FXMLLoader.load(getClass().getResource("/opencarshop/TelaPrincipal.fxml"));
-                    Scene scene = new Scene(root);
-                    Stage nStage = new Stage();
-                    nStage.setScene(scene);
-                    //nStage.setMaximized(true);
-                    nStage.setMaxHeight(768);
-                    nStage.setMaxWidth(1024);
-                    nStage.setTitle("OpenCarShop");
-                    nStage.setResizable(false);
-                    nStage.show();
-                    Stage stage = (Stage) cadastroLink.getScene().getWindow();
-                    stage.close();
-                } 
-                catch (IOException e) 
-                {
-                    e.printStackTrace();
-                }
-            }
-            else
-            {
-                labelErro.setText("Login ou senha errado!!!");
-            }
-        }
-        else
-        {
-            labelErro.setText("Login ou senha errado!!!");
-        }
-    }
-     */
+    
     @FXML
     private void cadastrar(ActionEvent event) throws ParseException {
-
-        Fornecedor fornecedor = new Fornecedor();
-        Endereco end = new Endereco();
-
-        FornecedorDAO f = new FornecedorDAO();
+    
+        Fornecedor  fornecedor    = new Fornecedor();
+        Endereco     end     = new Endereco();
+        FornecedorDAO f     = new FornecedorDAO();
         Utilidades u = new Utilidades();
-
+      
         // OBJETO FORNECEDOR
         fornecedor.setCnpj(tf_cnpjCadastro.getText());
         fornecedor.setRazaoSocial(tf_razaoCadastro.getText());
@@ -144,7 +102,7 @@ public class FornecedorController implements Initializable {
         fornecedor.setTelefone1(tf_telefone1Cadastro.getText());
         fornecedor.setTelefone2(tf_telefone2Cadastro.getText());
         fornecedor.setDescricao(tf_descricaoCadastro.getText());
-
+        fornecedor.setAtivo(true);
         // OBJETO ENDERECO
         end.setCEP(tf_cepCadastro.getText());
         end.setEstado(tf_estadoCadastro.getText());
@@ -155,19 +113,21 @@ public class FornecedorController implements Initializable {
         end.setComplemento(tf_complementoCadastro.getText());
         end.setTipo(cb_tipoCadastro.getValue().charAt(0));
 
-        if (f.cadastraFornecedor(fornecedor, end)) {
+        
+        
+        if(f.cadastraFornecedor(fornecedor, end))
+        {
             resultadoCadastro.setText("Cadastrado com sucesso!!");
-        } else {
+        }
+        else
+        {
             resultadoCadastro.setText("Erro ao cadastrar!! Tente novamente.");
         }
     }
-
-    @FXML
-    private void buscar(ActionEvent event) throws Exception {
-
-    }
-
-    private void carregaTabelaFornecedor() throws Exception {
+      
+    
+    private void carregaTabelaFornecedor() throws Exception
+    {
         col_cnpj.setCellValueFactory(new PropertyValueFactory<>("cnpj"));
         col_razaoSocial.setCellValueFactory(new PropertyValueFactory<>("razaoSocial"));
         col_email.setCellValueFactory(new PropertyValueFactory<>("email"));
@@ -181,15 +141,51 @@ public class FornecedorController implements Initializable {
         observableListFornecedor = FXCollections.observableArrayList(listaFornecedor);
         tbl_fornecedor.setItems(observableListFornecedor);
     }
-
+    
+     @FXML
+    private void alterarCadastro(ActionEvent event) throws Exception {
+        Fornecedor  fornecedor    = new Fornecedor();
+        fornecedor.setCnpj(tf_cnpjCadastro.getText());
+        fornecedor.setRazaoSocial(tf_razaoCadastro.getText());
+        fornecedor.setEmail(tf_emailCadastro.getText());
+        fornecedor.setTelefone1(tf_telefone1Cadastro.getText());
+        fornecedor.setTelefone2(tf_telefone2Cadastro.getText());
+        fornecedor.setDescricao(tf_descricaoCadastro.getText());
+        fornecedor.setAtivo(cb_ativo.isSelected());
+        
+        FornecedorDAO f     = new FornecedorDAO();
+        if(f.alteraFornecedor(fornecedor))
+        {
+            confirmaAlteracao.setText("Alteração realizada com sucesso!!");
+        }else
+        {
+            confirmaAlteracao.setText("Erro ao realizar a alteração!!");
+        }
+    }
+    
+    public void selecionarItemTablelaFornecedor(Fornecedor fornecedor){
+        if (fornecedor.getCnpj() != null) {
+              tf_cnpjCadastro.setText(fornecedor.getCnpj());
+              tf_razaoCadastro.setText(fornecedor.getRazaoSocial());
+              tf_emailCadastro.setText(fornecedor.getEmail());
+              tf_telefone1Cadastro.setText(fornecedor.getTelefone1());
+              tf_telefone2Cadastro.setText(fornecedor.getTelefone2());
+              tf_descricaoCadastro.setText(fornecedor.getDescricao());
+              cb_ativo.setSelected(fornecedor.getAtivo());
+              
+        } 
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
             carregaTabelaFornecedor();
+            tbl_fornecedor.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> selecionarItemTablelaFornecedor(newValue));
         } catch (Exception ex) {
-            //Logger.getLogger(FuncionarioController.class.getName()).log(Level.SEVERE, null, ex);
+         
         }
-
+        
     }
-
+    
 }
--- OpenCarShop/src/opencarshop/fornecedor/model/FornecedorDAO.java
+++ OpenCarShop/src/opencarshop/fornecedor/model/FornecedorDAO.java
@@ -8,8 +8,11 @@ package opencarshop.fornecedor.model;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import opencarshop.Endereco;
 import opencarshop.util.ConexaoMySQL;
 import opencarshop.util.Utilidades;
@@ -19,57 +22,68 @@ import opencarshop.util.Utilidades;
  * @author JomarR
  */
 public class FornecedorDAO {
-
-    public Fornecedor getFornecedor(String cnpj) {
+    
+public Fornecedor getFornecedor(String cnpj)
+    {
         ConexaoMySQL c = new ConexaoMySQL();
         Connection conn = null;
         PreparedStatement stmt = null;
         Utilidades u = new Utilidades();
-
+        
         String query = "SELECT * FROM Fornecedor WHERE cnpj=?";
-
+        
         Fornecedor fornecedor = new Fornecedor();
-        try {
+        try
+        {
             conn = c.conectar();
             stmt = conn.prepareStatement(query);
-            stmt.setString(1, cnpj);
-
+            stmt.setString(1,cnpj);
+            
             ResultSet resultado = stmt.executeQuery();
-
-            if (resultado.next()) {
+            
+            if(resultado.next())
+            {
                 fornecedor.setCnpj(resultado.getString("cnpj"));
                 fornecedor.setDescricao(resultado.getString("razaoSocial"));
                 fornecedor.setEmail(resultado.getString("email"));
                 fornecedor.setTelefone1(resultado.getString("telefone1"));
                 fornecedor.setTelefone2(resultado.getString("telefone2"));
-                fornecedor.setDescricao(resultado.getString("descricao"));
+                 fornecedor.setDescricao(resultado.getString("descricao"));
+                 fornecedor.setAtivo(resultado.getBoolean("ativo"));
             }
-
+            
             conn.close();
-        } catch (Exception e) {
+        }
+        catch(Exception e)
+        {
             e.printStackTrace();
         }
         return fornecedor;
     }
-
-    public boolean cadastraFornecedor(Fornecedor fornecedor, Endereco end) {
+    
+    public boolean cadastraFornecedor(Fornecedor fornecedor, Endereco end)
+    {
         ConexaoMySQL c = new ConexaoMySQL();
         Connection conn = null;
-
+        
         PreparedStatement stmtEnd = null;
-
+        
         PreparedStatement stmtFornecedor = null;
-
+        
         String queryEnd = "INSERT INTO Endereco (cep, estado, cidade, bairro, rua, numero, complemento,tipo) VALUES (?,?,?,?,?,?,?,?)";
-        String queryFornecedor = "INSERT INTO Fornecedor (cnpj, razaoSocial, email, telefone1, telefone2, descricao, endereco) VALUES (?,?,?,?,?,?,(select LAST_INSERT_ID()))";
-
-        try {
+        String queryFornecedor = "INSERT INTO Fornecedor (cnpj, razaoSocial, email, telefone1, telefone2, descricao, endereco, ativo) VALUES (?,?,?,?,?,?,(select LAST_INSERT_ID()),?)";
+       
+        
+        try
+        {
             conn = c.conectar();
             conn.setAutoCommit(false);
-
+            
             stmtEnd = conn.prepareStatement(queryEnd);
             stmtFornecedor = conn.prepareStatement(queryFornecedor);
-
+            
+            
+            
             stmtEnd.setString(1, end.getCEP());
             stmtEnd.setString(2, end.getEstado());
             stmtEnd.setString(3, end.getCidade());
@@ -78,28 +92,36 @@ public class FornecedorDAO {
             stmtEnd.setInt(6, end.getNumero());
             stmtEnd.setString(7, end.getComplemento());
             stmtEnd.setString(8, Character.toString(end.getTipo()));
-
+            
             stmtFornecedor.setString(1, fornecedor.getCnpj());
             stmtFornecedor.setString(2, fornecedor.getRazaoSocial());
             stmtFornecedor.setString(3, fornecedor.getEmail());
             stmtFornecedor.setString(4, fornecedor.getTelefone1());
             stmtFornecedor.setString(5, fornecedor.getTelefone2());
             stmtFornecedor.setString(6, fornecedor.getDescricao());
+            stmtFornecedor.setBoolean(7, true);
+            
 
+          
+            
             stmtEnd.execute();
             stmtFornecedor.execute();
-
+            
+            
             conn.commit();
-
+            
             conn.close();
             return true;
-        } catch (Exception e) {
+        }
+        catch(Exception e)
+        {
             e.printStackTrace();
             return false;
         }
     }
-
-    public List<Fornecedor> getAllFornecedor() throws Exception {
+    
+    public List<Fornecedor> getAllFornecedor() throws Exception
+    {
         String query = "SELECT * FROM Fornecedor";
         List<Fornecedor> retorno = new ArrayList<>();
         Utilidades u = new Utilidades();
@@ -117,7 +139,7 @@ public class FornecedorDAO {
                 fornecedor.setTelefone1(resultado.getString("telefone1"));
                 fornecedor.setTelefone2(resultado.getString("telefone2"));
                 fornecedor.setDescricao(resultado.getString("descricao"));
-
+                fornecedor.setAtivo(resultado.getBoolean("ativo"));
                 retorno.add(fornecedor);
             }
         } catch (Exception e) {
@@ -126,4 +148,34 @@ public class FornecedorDAO {
         conn.close();
         return retorno;
     }
+    
+    public Boolean alteraFornecedor(Fornecedor fornecedor) throws SQLException
+    {
+        String query = "UPDATE Fornecedor SET cnpj=?, razao=?, email=?, telefone1=?, telefone2=?, descricao =?, ativo=? WHERE cnpj=?";
+        
+        ConexaoMySQL c = new ConexaoMySQL();
+        Connection conn = null;
+        try {
+            conn = c.conectar();
+            PreparedStatement stmt = conn.prepareStatement(query);
+        
+     
+            stmt.setString(1, fornecedor.getCnpj());
+            stmt.setString(2, fornecedor.getRazaoSocial());
+            stmt.setString(4, fornecedor.getEmail());
+            stmt.setString(5, fornecedor.getTelefone1());
+            stmt.setString(6, fornecedor.getTelefone2());
+            stmt.setString(7, fornecedor.getDescricao());
+            stmt.setBoolean(8, fornecedor.getAtivo());
+            stmt.setString(9, fornecedor.getCnpj());
+            stmt.execute();
+            conn.close();
+            return true;
+        } catch (Exception ex) {
+            
+            Logger.getLogger(FornecedorDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
 }
+
